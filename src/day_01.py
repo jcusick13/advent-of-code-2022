b2ebded9795126b3f79590d471ceb4d883404e10
jcusick13 <jonathan.cusick09@gmail.com>
2022-12-01 07:34:58 -0500

    Finish day one

--- src/day_01.py
+++ src/day_01.py
@@ -4,26 +4,44 @@ from src.base import Solution
 class Day01(Solution):
     name = "01"
 
-    def read(self, part: int) -> list[int]:
+    def read(self, part: int) -> list[str]:
         with open(f"inputs/{self.name}_{part}.txt", encoding="utf-8") as f:
             puzzle_input = f.readlines()
         return puzzle_input
 
-    def part_one(self, puzzle_input: list[int]) -> int:
-        depth_increases: int = 0
+    def part_one(self, calorie_list: list[str]) -> int:
+        max_calories: int = 0
+        current_calories: int = 0
 
-        for i, depth in enumerate(puzzle_input):
-            if i == 0:
-                prev = depth
-                continue
+        for entry in calorie_list:
+            try:
+                current_calories += int(entry.strip())
 
-            if depth > prev:
-                depth_increases += 1
+            except ValueError:
+                # Encountered empty entry in list, reset counter
+                # for a new elf
+                max_calories = max(max_calories, current_calories)
+                current_calories = 0
 
-            prev = depth
+        return max_calories
 
-        return depth_increases
+    def part_two(self, calorie_list: list[str]) -> int:
+        elf_totals: list[int] = []
+        current_calories: int = 0
+
+        for entry in calorie_list:
+            try:
+                current_calories += int(entry.strip())
+
+            except ValueError:
+                # Encountered empty entry in list, reset counter
+                # for a new elf
+                elf_totals.append(current_calories)
+                current_calories = 0
+
+        # Total the three highest calorie counts
+        return sum(sorted(elf_totals, reverse=True)[:3])
 
 
 if __name__ == '__main__':
-    Day01().solve(1)
+    Day01().solve()
